77c707533d9631f35b186e4c4a08daf2c8f79334
mlbb <polooperatoosh@gmail.com>
2025-11-18 00:48:51 +0300

    renamed app.dart app title & working adding patients

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Modern CRUD App',
+      title: 'Dentista',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
